33a1bc7a7a1c7d536f7ff730576041df51b4607b
Taylor Denouden <taylordenouden@gmail.com>
2020-04-24 14:36:29 -0400

    Set noData value as 0 for image labels

--- utils/data_prep.py
+++ utils/data_prep.py
@@ -1,10 +1,11 @@
-from osgeo import gdal, osr, ogr
-import rasterio
 from pathlib import Path
+
 import numpy as np
+import rasterio
+from PIL import Image
+from osgeo import gdal, osr, ogr
 from tqdm.auto import tqdm
 from tqdm.contrib.itertools import product as tproduct
-from PIL import Image
 
 
 def check_same_extent(src_a, src_b):
@@ -60,7 +61,7 @@ def slice_and_dice_image(src_img, dest_d, mode='L', crop_size=200):
         dest_d = Path(dest_d)
         for i, (x0, y0) in enumerate(tproduct(x0s, y0s)):
             dest = str(dest_d.joinpath(f"{i}.png"))
-            window = ((y0, y0+crop_size), (x0, x0+crop_size))
+            window = ((y0, y0 + crop_size), (x0, x0 + crop_size))
 
             if mode == 'L':
                 subset = dataset.read(1, window=window)
@@ -131,7 +132,8 @@ def clip_raster_by_extent(dest, src, extent, height, width):
         height: The height in pixels of the clipped raster
     Returns: None
     """
-    opts = gdal.TranslateOptions(format="GTiff", projWin=extent, outputBounds=extent, height=height, width=width)
+    opts = gdal.TranslateOptions(format="GTiff", projWin=extent, outputBounds=extent, height=height, width=width,
+                                 noData=0)
     ds = gdal.Translate(dest, src, options=opts)
     ds = None
     opts = None
@@ -281,4 +283,5 @@ def del_extra_labels(dataset):
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
