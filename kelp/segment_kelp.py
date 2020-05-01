56d8d9a77349dc925848adad2859cb2e609a46be
Taylor Denouden <taylordenouden@gmail.com>
2020-05-01 17:04:49 -0400

    Change LR scheduler

--- kelp/segment_kelp.py
+++ kelp/segment_kelp.py
@@ -235,7 +235,7 @@ def train(train_data_dir, eval_data_dir, checkpoint_dir,
     dataloaders = get_dataloaders("train", train_data_dir, eval_data_dir, batch_size=batch_size)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.9)
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=2)
 
     checkpoint_path = checkpoint_dir.joinpath(f'{MODEL_NAME}_checkpoint.pt')
 
