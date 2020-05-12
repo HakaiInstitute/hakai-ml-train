36602be3509e296487b79e62dbcca3717b978825
Taylor Denouden <taylordenouden@gmail.com>
2020-05-12 12:41:36 -0400

    Create job scheduler script
    Allows adding and removing jobs while others are running and logs success or failure

---
+++ job_queue/run_jobs.py
@@ -0,0 +1,75 @@
+import os
+import subprocess
+from collections import deque
+from pathlib import Path
+from sqlite3 import connect
+from time import sleep
+
+
+class SQLite(object):
+    def __init__(self, db_path):
+        self.db_path = db_path
+
+    def __enter__(self):
+        self.db = connect(self.db_path)
+        return self.db
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.db.close()
+
+
+def get_jobs(db):
+    curs = db.cursor()
+    curs.execute("SELECT pk, infile, outfile, weightfile FROM jobs WHERE status='scheduled' ORDER BY created_dt;")
+    return curs.fetchall()
+
+
+def mark_job_complete(db, pk, success=True):
+    status = 'complete' if success else 'failed'
+    db.execute(f"UPDATE jobs SET status='{status}' WHERE pk={pk};")
+    return db.commit()
+
+
+def job_loop(db):
+    q = deque()
+
+    while True:
+        if len(q) == 0:
+            # Add items to q
+            new_jobs = get_jobs(db)
+            if len(new_jobs) > 0:
+                for job in new_jobs:
+                    q.append(job)
+            else:
+                sleep(60)  # Wait a minute
+
+        else:
+            # Process jobs using docker script
+            pk, in_file, out_file, weight_file = q.popleft()
+            print(f"Running job: {pk}")
+
+            if not all([Path(f).is_file() for f in [in_file, out_file, weight_file]]):
+                print(f"Invalid path specified for job {pk}")
+                mark_job_complete(db, pk, success=False)
+                continue
+
+            subprocess.run([
+                "docker", "run", "--rm",
+                f"-v", f"{in_file}:{in_file}:ro",
+                f"-v", f"{Path(weight_file).parent}:{Path(weight_file).parent}:ro",
+                "-v", f"{Path(out_file).parent}:{Path(out_file).parent}",
+                f"--user={os.geteuid()}:{os.getgid()}",
+                "--ipc=host",
+                "--gpus=all",
+                "--name=kelp-pred",
+                "tayden/deeplabv3-kelp", "pred",
+                f"--seg_in={in_file}", f"--seg_out={out_file}", f"--weights={weight_file}"
+            ], cwd='../', check=True)
+
+            # Mark job complete
+            mark_job_complete(db, pk)
+
+
+if __name__ == '__main__':
+    with SQLite("kelp.sqlite") as db:
+        job_loop(db)
