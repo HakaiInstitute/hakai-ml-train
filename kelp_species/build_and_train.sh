85e4df08049130c44dea8e9ee39dbd1a167e5ea5
Taylor Denouden <taylordenouden@gmail.com>
2020-06-08 11:41:56 -0700

    Fix AWS scripts, increase batch size

--- kelp_species/build_and_train.sh
+++ kelp_species/build_and_train.sh
@@ -27,7 +27,7 @@ docker run -dit --rm \
   --name kelp-species-train \
   tayden/deeplabv3-kelp-species train "/opt/ml/input/data/train" "/opt/ml/input/data/eval" "/opt/ml/output/checkpoints" \
   --name=$NAME --epochs=150 --unfreeze_backbone_epoch=30 --lr=0.0003019951720402019 --weight_decay=0.001  \
-  --gradient_clip_val=0.5 --batch_size=2 --accumulate_grad_batches=4 --precision=16 --amp_level="O1" --auto_lr_find
+  --gradient_clip_val=0.5 --batch_size=8 --accumulate_grad_batches=4 --precision=16 --amp_level="O1" --auto_lr_find
 
 # Can start tensorboard in running container as follows:
 docker exec -dit kelp-species-train tensorboard --logdir=/opt/ml/output/checkpoints --host=0.0.0.0 --port=$PORT
