80f744d6bf872d0df3933c40a586709439276014
Taylor Denouden <taylordenouden@gmail.com>
2020-06-08 11:14:08 -0700

    Fix bugs in bash script

--- kelp_species/build_and_train.sh
+++ kelp_species/build_and_train.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Get the path to this script
-NAME=AMPO1
+NAME=FIRST
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 PORT=6006
 
 # Build the docker image
-DOCKER_BUILDKIT=1 docker build --file ./Dockerfile --compress --tag tayden/deeplabv3-kelp-species ..
+DOCKER_BUILDKIT=1 docker build --file ./Dockerfile --tag tayden/deeplabv3-kelp-species ..
 
 # Sync datasets
-aws s3 sync s3://hakai-deep-learning-datasets/kelp/train ./train_input/data/train
-aws s3 sync s3://hakai-deep-learning-datasets/kelp/eval ./train_input/data/eval
+# For testing, add: --exclude="*" --include="**/[0-9].png"
+aws s3 sync s3://hakai-deep-learning-datasets/kelp_species/train ./train_input/data/train
+aws s3 sync s3://hakai-deep-learning-datasets/kelp_species/eval ./train_input/data/eval
 
 # Make output dirs
 mkdir -p "./train_output/checkpoints"
@@ -25,15 +26,15 @@ docker run -dit --rm \
   --gpus all \
   --name kelp-species-train \
   tayden/deeplabv3-kelp-species train "/opt/ml/input/data/train" "/opt/ml/input/data/eval" "/opt/ml/output/checkpoints" \
-  --accumulate_grad_batches=4 --gradient_clip_val=0.5 --weight_decay=0.001 --unfreeze_backbone_epoch=30 --epochs=150 \
-  --name=$NAME --precision=16 --amp_level="O1" --lr=0.0003019951720402019
+  --name=$NAME --epochs=150 --unfreeze_backbone_epoch=30 --lr=0.0003019951720402019 --weight_decay=0.001  \
+  --gradient_clip_val=0.5 --batch_size=2 --accumulate_grad_batches=4 --precision=16 --amp_level="O1" --auto_lr_find
 
 # Can start tensorboard in running container as follows:
-docker exec -dit kelp-train tensorboard --logdir=/opt/ml/output/checkpoints --host=0.0.0.0 --port=$PORT
+docker exec -dit kelp-species-train tensorboard --logdir=/opt/ml/output/checkpoints --host=0.0.0.0 --port=$PORT
 # Navigate to localhost:6006 to see train stats
 
 # Wait for process so AWS exits when it's done
-docker wait kelp-train
+docker wait kelp-species-train
 
 # Sync results to S3
 ARCHIVE="$(date +'%Y-%m-%d-%H%M')_$NAME.tar.gz"
