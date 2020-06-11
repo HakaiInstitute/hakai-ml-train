a5ca21df6f71d51c08046c15857916fad3c247cf
Taylor Denouden <taylordenouden@gmail.com>
2020-06-11 10:28:50 -0400

    Change batch size and unfreeze layers from start.

--- kelp_species/build_and_train.sh
+++ kelp_species/build_and_train.sh
@@ -26,8 +26,8 @@ docker run -dit --rm \
   --gpus all \
   --name kelp-species-train \
   tayden/deeplabv3-kelp-species train "/opt/ml/input/data/train" "/opt/ml/input/data/eval" "/opt/ml/output/checkpoints" \
-  --name=$NAME --epochs=100 --unfreeze_backbone_epoch=30 --lr=0.001 --weight_decay=0.001  \
-  --gradient_clip_val=0.5 --batch_size=4 --accumulate_grad_batches=8 --precision=16 --amp_level="O1"
+  --name=$NAME --epochs=100 --auto_scale_batch_size --lr=0.001 --weight_decay=0.001  \
+  --gradient_clip_val=0.5 --batch_size=4 --precision=16 --amp_level="O1"
 
 # Can start tensorboard in running container as follows:
 docker exec -dit kelp-species-train tensorboard --logdir=/opt/ml/output/checkpoints --host=0.0.0.0 --port=$PORT
--- kelp_species/deeplabv3_lightning.py
+++ kelp_species/deeplabv3_lightning.py
@@ -44,9 +44,8 @@ class DeepLabv3Model(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.hparams.lr, weight_decay=self.hparams.weight_decay)
 
-        steps_per_epoch = math.floor(len(self.train_dataloader()) / max(self.hparams.gpus, 1))
-        pct_start = self.hparams.unfreeze_backbone_epoch / self.hparams.epochs
-        lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=self.hparams.lr, pct_start=pct_start,
+        steps_per_epoch = math.floor(len(self.train_dataloader()))
+        lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=self.hparams.lr,
                                                            epochs=self.hparams.epochs, steps_per_epoch=steps_per_epoch)
 
         return [optimizer], [{'scheduler': lr_scheduler, 'interval': 'step'}]
@@ -140,7 +139,7 @@ def _get_checkpoint(checkpoint_dir, name=""):
 def train(train_data_dir, val_data_dir, checkpoint_dir,
           num_classes=3, batch_size=4, lr=0.001, weight_decay=1e-4, epochs=310, aux_loss_factor=0.3,
           accumulate_grad_batches=1, gradient_clip_val=0, precision=32, amp_level='O1', auto_lr_find=False,
-          unfreeze_backbone_epoch=150, auto_scale_batch_size=False, name=""):
+          unfreeze_backbone_epoch=0, auto_scale_batch_size=False, name=""):
     """
     Train the DeepLabV3 Kelp Detection model.
     Args:
@@ -191,7 +190,6 @@ def train(train_data_dir, val_data_dir, checkpoint_dir,
         precision=precision,
         amp_level=amp_level,
         unfreeze_backbone_epoch=unfreeze_backbone_epoch,
-        gpus=torch.cuda.device_count(),
     )
 
     trainer_kwargs = {
