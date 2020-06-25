1d95686bb3f8335ec3146797451df18641e63ff8
Taylor Denouden <taylordenouden@gmail.com>
2020-06-25 16:05:00 -0400

    Change logged names

--- kelp/model.py
+++ kelp/model.py
@@ -6,14 +6,14 @@ from models.deeplabv3 import DeepLabv3
 class KelpModel(DeepLabv3):
     def end_epoch_stats(self, losses, ious, phase):
         bg_iou = ious[:, 0][~torch.isnan(ious[:, 0])].mean(dim=0)
-        kelp_iou = ious[:, 1][~torch.isnan(ious[:, 1])].mean(dim=0)
+        fg_iou = ious[:, 1][~torch.isnan(ious[:, 1])].mean(dim=0)
 
-        miou = torch.stack([bg_iou, kelp_iou]).mean()
+        miou = torch.stack([bg_iou, fg_iou]).mean()
 
         key_prefix = 'val_' if phase == 'val' else ''
         return {
             f'{key_prefix}loss': losses.mean(),
             f'{key_prefix}miou': miou,
             f'{key_prefix}bg_iou': bg_iou,
-            f'{key_prefix}kelp_iou': kelp_iou
+            f'{key_prefix}fg_iou': fg_iou
         }
