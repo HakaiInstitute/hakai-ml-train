70111ecfb44e7887707aa170f929977310d41389
Taylor Denouden <taylordenouden@gmail.com>
2020-08-04 12:51:35 -0400

    Fix data loading bug

--- utils/dataset/SegmentationDataset.py
+++ utils/dataset/SegmentationDataset.py
@@ -24,8 +24,6 @@ class SegmentationDataset(Dataset):
 
         seed = np.random.randint(2147483647)
 
-        img = self.pad_out(img)
-
         torch.manual_seed(seed)
         random.seed(seed)  # apply this seed to img transforms
         if self.transform is not None:
