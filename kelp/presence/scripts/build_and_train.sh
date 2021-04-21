aff741be04641c6f676336eb5ea79d325222972d
Taylor Denouden <taylordenouden@gmail.com>
2021-04-21 13:16:56 -0700

    Don't train batchnorm in backbone layers

--- kelp/presence/scripts/build_and_train.sh
+++ kelp/presence/scripts/build_and_train.sh
@@ -34,7 +34,7 @@ docker run -dit --rm \
   tayden/deeplabv3-kelp train /opt/ml/input/data /opt/ml/output/checkpoints \
   --name=$NAME --num_classes=2 \
   --lr=0.001 --weight_decay=0.001 --gradient_clip_val=0.5 \
-  --auto_select_gpus --gpus=-1 --benchmark --sync_batchnorm --train_backbone_bn \
+  --auto_select_gpus --gpus=-1 --benchmark --sync_batchnorm --no_train_backbone_bn \
   --max_epochs=100 --batch_size=8 --amp_level=O2 --precision=16 --distributed_backend=ddp --log_every_n_steps=10  # AWS
 #  --max_epochs=100 --batch_size=8 --unfreeze_backbone_epoch=100 --log_every_n_steps=5 --overfit_batches=2  # TESTING
 
