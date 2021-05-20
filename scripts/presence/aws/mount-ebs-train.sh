693d673ec4098f4b3cbbcd2c6af5c66cd6ea047d
Taylor Denouden <taylordenouden@gmail.com>
2021-05-20 11:51:26 -0700

    Fix aws launch

--- scripts/presence/aws/mount-ebs-train.sh
+++ scripts/presence/aws/mount-ebs-train.sh
@@ -56,7 +56,6 @@ if [ $VOLUME_ID ]; then
 		mount --bind /dltraining/presence/train_output ./train_output
 
 		# Initiate training using the pytorch_36 conda environment
-		cd scripts || exit 1
 		sudo -H -u ubuntu bash -c "source /home/ubuntu/anaconda3/bin/activate python3; bash ./build_and_train.sh"
 fi
 
