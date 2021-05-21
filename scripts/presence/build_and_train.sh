f86619c5219d3110084a7c18cc7b370906b767b2
Taylor Denouden <taylordenouden@gmail.com>
2021-05-21 11:05:27 -0700

    Fix presence train cmd

--- scripts/presence/build_and_train.sh
+++ scripts/presence/build_and_train.sh
@@ -37,8 +37,8 @@ docker run -dit --rm \
   --name=$NAME --num_classes=2 \
   --lr=0.001 --backbone_lr=0.0001 --weight_decay=0.001 --gradient_clip_val=0.5 \
   --auto_select_gpus --gpus=-1 --benchmark --sync_batchnorm \
-  --max_epochs=100 --batch_size=8 --amp_level=O2 --precision=16 --accelerator=ddp --log_every_n_steps=10  --no_train_backbone_bn # AWS
-#  --max_epochs=10 --batch_size=2 --unfreeze_backbone_epoch=100 --log_every_n_steps=5 --overfit_batches=2  # TESTING
+  --max_epochs=100 --batch_size=8 --amp_level=O2 --precision=16 --accelerator=ddp --log_every_n_steps=10  # AWS
+#  --max_epochs=10 --batch_size=2 --unfreeze_backbone_epoch=100 --log_every_n_steps=5 --overfit_batches=2 --no_train_backbone_bn # TESTING
 
 # L-RASPP MobileNet v3
 #docker run -dit --rm \
