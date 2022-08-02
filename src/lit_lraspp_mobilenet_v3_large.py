40e11025de27661f053cff583d376ddb0bcc2a9b
Taylor Denouden <taylordenouden@gmail.com>
2022-08-02 10:50:10 -0700

    Setup optuna optimizer
    Doesn't work with lightning 1.6, so had to downgrade to 1.5

--- src/lit_lraspp_mobilenet_v3_large.py
+++ src/lit_lraspp_mobilenet_v3_large.py
@@ -2,19 +2,17 @@
 # Organization: Hakai Institute
 # Date: 2021-05-17
 # Description:
+import argparse
 import os
 from argparse import ArgumentParser
 from pathlib import Path
 from typing import Any, Optional
 
+import optuna
 import pytorch_lightning as pl
 import torch
+from optuna.integration import PyTorchLightningPruningCallback
 from pytorch_lightning.loggers import TensorBoardLogger
-from ray import tune
-from ray.tune import CLIReporter
-from ray.tune.integration.pytorch_lightning import TuneReportCheckpointCallback
-from ray.tune.schedulers import ASHAScheduler
-from ray.tune.suggest.optuna import OptunaSearch
 from torchmetrics import Accuracy, JaccardIndex, Precision, Recall
 from torchvision.models.segmentation import lraspp_mobilenet_v3_large
 
@@ -24,12 +22,13 @@ from utils.loss import FocalTverskyLoss
 
 class LRASPPMobileNetV3Large(pl.LightningModule):
     def __init__(self, num_classes: int = 2, ignore_index: Optional[int] = None, lr: float = 0.35,
-                 weight_decay: float = 0, loss_alpha: float = 0.7, loss_gamma: float = 4.0 / 3.0):
+                 weight_decay: float = 0, loss_alpha: float = 0.7, loss_gamma: float = 4.0 / 3.0, max_epochs: int = 100):
         super().__init__()
         self.num_classes = num_classes
         self.ignore_index = ignore_index
         self.lr = lr
         self.weight_decay = weight_decay
+        self.max_epochs = max_epochs
 
         # Create model from pre-trained DeepLabv3
         self.model = lraspp_mobilenet_v3_large(progress=True, num_classes=self.num_classes)
@@ -118,16 +117,36 @@ class LRASPPMobileNetV3Large(pl.LightningModule):
                 m.train()
 
         # take average of `self.mc_iteration` iterations
-        pred = torch.vstack([self.forward(batch).unsqueeze(0) for _ in range(mc_iteration)]).mean(
-            dim=0)
+        pred = torch.vstack([self.forward(batch).unsqueeze(0) for _ in range(mc_iteration)]).mean(dim=0)
         return pred
+
+    @property
+    def estimated_stepping_batches(self) -> int:
+        """Total training steps inferred from datamodule and devices."""
+        if self.trainer.max_steps != -1:
+            return self.trainer.max_steps
+
+        limit_batches = self.trainer.limit_train_batches
+        batches = len(self.trainer.datamodule.train_dataloader())
+        batches = (
+            min(batches, limit_batches)
+            if isinstance(limit_batches, int)
+            else int(limit_batches * batches)
+        )
+
+        num_devices = max(1, self.trainer.num_gpus, self.trainer.num_processes)
+        if self.trainer.tpu_cores:
+            num_devices = max(num_devices, self.trainer.tpu_cores)
+
+        effective_accum = self.trainer.accumulate_grad_batches * num_devices
+        return (batches // effective_accum) * self.max_epochs
 
     def configure_optimizers(self):
         """Init optimizer and scheduler"""
         optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, self.parameters()),
                                     lr=self.lr, weight_decay=self.weight_decay, nesterov=True, momentum=0.9)
         lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=self.lr,
-                                                           total_steps=self.trainer.estimated_stepping_batches)
+                                                           total_steps=self.estimated_stepping_batches)
         return [optimizer], [{"scheduler": lr_scheduler, "interval": "step"}]
 
     @staticmethod
@@ -161,90 +180,104 @@ class Finetuning(pl.callbacks.BaseFinetuning):
             )
 
 
-def train(config, args):
-    pl.seed_everything(0)
+# Objective function to be maximized by Optuna
+class Objective(object):
+    def __init__(self, args: argparse.Namespace):
+        self.args = args
 
-    # ------------
-    # data
-    # ------------
-    kelp_data = KelpDataModule(
-        args.data_dir,
-        num_classes=args.num_classes,
-        batch_size=args.batch_size
-    )
+    def __call__(self, trial: optuna.trial.Trial):
+        args = self.args
 
-    # ------------
-    # model
-    # ------------
-    model = LRASPPMobileNetV3Large(
-        num_classes=args.num_classes,
-        ignore_index=args.ignore_index,
-        lr=config['lr'],
-        loss_alpha=config['alpha'],
-        weight_decay=config['weight_decay'],
-        # loss_gamma=config['gamma']
-    )
+        # ------------
+        # data
+        # ------------
+        kelp_data = KelpDataModule(
+            args.data_dir,
+            # num_workers=0,
+            # pin_memory=False,
+            num_classes=args.num_classes,
+            batch_size=args.batch_size
+        )
 
-    if args.weights and Path(args.weights).suffix == ".pt":
-        print("Loading state_dict:", args.weights)
-        weights = torch.load(args.weights)
+        # ------------
+        # hyperparameter search space
+        # ------------
+        lr = trial.suggest_float('lr', args.min_lr, args.max_lr, log=True)
+        alpha = trial.suggest_float('alpha', args.min_alpha, args.max_alpha)
+        weight_decay = trial.suggest_float('weight_decay', args.min_weight_decay, args.max_weight_decay)
 
-        # Remove trained weights for previous classifier output layers
-        if args.drop_output_layer_weights:
-            del weights["model.classifier.low_classifier.weight"]
-            del weights["model.classifier.low_classifier.bias"]
-            del weights["model.classifier.high_classifier.weight"]
-            del weights["model.classifier.high_classifier.bias"]
+        # ------------
+        # model
+        # ------------
+        model = LRASPPMobileNetV3Large(
+            num_classes=args.num_classes,
+            ignore_index=args.ignore_index,
+            lr=lr,
+            loss_alpha=alpha,
+            weight_decay=weight_decay,
+            max_epochs=args.max_epochs,
+        )
 
-        model.load_state_dict(weights, strict=False)
+        if args.weights and Path(args.weights).suffix == ".pt":
+            print("Loading state_dict:", args.weights)
+            weights = torch.load(args.weights)
 
-    elif args.weights and Path(args.weights).suffix == ".ckpt":
-        print("Loading checkpoint:", args.weights)
-        model = LRASPPMobileNetV3Large.load_from_checkpoint(args.weights)
+            # Remove trained weights for previous classifier output layers
+            if args.drop_output_layer_weights:
+                del weights["model.classifier.low_classifier.weight"]
+                del weights["model.classifier.low_classifier.bias"]
+                del weights["model.classifier.high_classifier.weight"]
+                del weights["model.classifier.high_classifier.bias"]
 
-    # ------------
-    # callbacks
-    # ------------
-    callbacks = [
-        TuneReportCheckpointCallback(
-            metrics=dict(
-                [
-                    ("loss", "val_loss"),
-                    ("miou", "val_miou"),
-                    ("accuracy", "val_accuracy"),
-                    ("precision", "val_precision"),
-                    ("recall", "val_recall"),
-                ] + [
-                    (f"cls{i}_iou", f"val_cls{i}_iou") for i in
-                    filter(lambda i: i != args.ignore_index,
-                           range(args.num_classes))
-                ]),
-            filename="checkpoint",
-            on="validation_end"
-        ),
-        # pl.callbacks.LearningRateMonitor()
-    ]
+            model.load_state_dict(weights, strict=False)
 
-    if args.backbone_finetuning_epoch is not None:
-        callbacks.append(Finetuning(unfreeze_at_epoch=args.backbone_finetuning_epoch))
-    if args.swa_epoch_start:
-        callbacks.append(
-            pl.callbacks.StochasticWeightAveraging(swa_lrs=args.swa_lrs, swa_epoch_start=args.swa_epoch_start))
+        elif args.weights and Path(args.weights).suffix == ".ckpt":
+            print("Loading checkpoint:", args.weights)
+            model = LRASPPMobileNetV3Large.load_from_checkpoint(args.weights)
 
-    tensorboard_logger = TensorBoardLogger(save_dir=tune.get_trial_dir(), name="", version=".", default_hp_metric=False)
+        # ------------
+        # callbacks
+        # ------------
+        checkpoint_callback = pl.callbacks.ModelCheckpoint(
+            # verbose=True,
+            monitor="val_miou", mode="max",
+            filename="best-{val_miou:.4f}-{epoch}",
+            save_top_k=1, save_last=True,
+            save_on_train_epoch_end=False,
+            every_n_epochs=1,
+        )
+        callbacks = [
+            checkpoint_callback,
+            pl.callbacks.LearningRateMonitor(),
+            PyTorchLightningPruningCallback(trial, monitor='val_miou'),
+        ]
 
-    trainer = pl.Trainer.from_argparse_args(
-        args,
-        logger=tensorboard_logger,
-        callbacks=callbacks,
-        enable_progress_bar=False
-    )
+        if args.backbone_finetuning_epoch is not None:
+            callbacks.append(Finetuning(unfreeze_at_epoch=args.backbone_finetuning_epoch))
+        if args.swa_epoch_start:
+            callbacks.append(
+                pl.callbacks.StochasticWeightAveraging(swa_lrs=args.swa_lrs, swa_epoch_start=args.swa_epoch_start))
 
-    trainer.fit(model, datamodule=kelp_data)
+        logger = TensorBoardLogger(save_dir=args.checkpoint_dir, name=f'{args.name}/trial_{trial.number}', default_hp_metric=False)
+        trainer = pl.Trainer.from_argparse_args(
+            args,
+            logger=logger,
+            callbacks=callbacks,
+        )
+        trainer.logger.log_hyperparams({
+            'lr': lr,
+            'alpha': alpha,
+            'weight_decay': weight_decay,
+            'batch_size': args.batch_size,
+        })
+        trainer.fit(model, datamodule=kelp_data)
+
+        return checkpoint_callback.best_model_score.detach().cpu()
 
 
 def cli_main(argv=None):
     pl.seed_everything(0)
+    # torch.multiprocessing.set_sharing_strategy('file_system')
 
     # ------------
     # args
@@ -296,51 +329,36 @@ def cli_main(argv=None):
     parser = pl.Trainer.add_argparse_args(parser)
     args = parser.parse_args(argv)
 
-    # ------------
-    # training
-    # ------------
-    search_space = {
-        "alpha": tune.uniform(args.min_alpha, args.max_alpha),
-        # "gamma": tune.choice([4.0 / 3.0]),
-        "lr": tune.loguniform(args.min_lr, args.max_lr),
-        "weight_decay": tune.uniform(args.min_weight_decay, args.max_weight_decay),
-    }
-    scheduler = ASHAScheduler(
-        max_t=args.max_epochs
-    )
-    reporter = CLIReporter(
-        parameter_columns=["lr", "alpha", "weight_decay"],
-        metric_columns=(["miou", "loss", "training_iteration"] + [f"cls{i}_iou" for i in
-                                                                  filter(lambda i: i != args.ignore_index,
-                                                                         range(args.num_classes))]),
-    )
+    # Make checkpoint directory
+    Path(args.checkpoint_dir, args.name).mkdir(exist_ok=True, parents=True)
 
-    train_fn_with_parameters = tune.with_parameters(train, args=args)
+    objective = Objective(args)
+    pruner: optuna.pruners.BasePruner = optuna.pruners.SuccessiveHalvingPruner()
 
-    analysis = tune.run(
-        train_fn_with_parameters,
-        resources_per_trial={"cpu": os.cpu_count(), "gpu": 1},
-        metric="miou",
-        mode="max",
-        config=search_space,
-        search_alg=OptunaSearch(
-            points_to_evaluate=[{'lr': args.init_lr, 'alpha': args.init_alpha, 'weight_decay': args.init_weight_decay}]),
-        num_samples=args.tune_trials,
-        scheduler=scheduler,
-        progress_reporter=reporter,
-        name=args.name,
-        local_dir=args.checkpoint_dir,
-    )
+    study = optuna.create_study(direction="maximize", pruner=pruner, storage=f'sqlite:///{args.checkpoint_dir}/{args.name}/hyper_opt.db')
+    study.enqueue_trial({'lr': args.init_lr, 'alpha': args.init_alpha, 'weight_decay': args.init_weight_decay})
+    study.optimize(objective, n_trials=args.tune_trials, gc_after_trial=True)
 
-    print("Best hyperparameters found were: ", analysis.best_config)
+    print("Number of finished trials: {}".format(len(study.trials)))
 
-    checkpoint_path = os.path.join(analysis.best_checkpoint.local_path, "checkpoint")
-    checkpoint = torch.load(checkpoint_path)
+    print("Best trial:")
+    best_trial = study.best_trial
 
-    save_path_name = Path(args.checkpoint_dir).joinpath(f"best-miou={analysis.best_result['miou']:.4f}")
-    torch.save(checkpoint, save_path_name.with_suffix(".ckpt"))
-    torch.save(checkpoint['state_dict'], save_path_name.with_suffix(".pt"))
-    analysis.best_result_df.to_csv(save_path_name.with_suffix(".csv"))
+    print("  Value: {}".format(best_trial.value))
+
+    print("  Params: ")
+    for key, value in best_trial.params.items():
+        print("    {}: {}".format(key, value))
+
+    # print("Best hyperparameters found were: ", analysis.best_config)
+
+    # checkpoint_path = os.path.join(analysis.best_checkpoint.local_path, "checkpoint")
+    # checkpoint = torch.load(checkpoint_path)
+
+    # save_path_name = Path(args.checkpoint_dir).joinpath(f"best-miou={analysis.best_result['miou']:.4f}")
+    # torch.save(checkpoint, save_path_name.with_suffix(".ckpt"))
+    # torch.save(checkpoint['state_dict'], save_path_name.with_suffix(".pt"))
+    # analysis.best_result_df.to_csv(save_path_name.with_suffix(".csv"))
 
     # if args.test_only:
     #     with torch.no_grad():
@@ -355,14 +373,10 @@ if __name__ == "__main__":
     debug = os.getenv("DEBUG", False)
     if debug:
         cli_main([
-            "/home/taylor/PycharmProjects/hakai-ml-train/data/kelp_species",
-            "/home/taylor/PycharmProjects/hakai-ml-train/checkpoints/kelp_species",
-            "--name=lr_aspp_kelp_species_DEV",
-            "--num_classes=4",
-            "--ignore_index=1",
-            "--weights=/home/taylor/PycharmProjects/hakai-ml-train/checkpoints/kelp_pa/LRASPP/"
-            "best-val_miou=0.8023-epoch=18-step=17593.pt",
-            "--drop_output_layer_weights",
+            "/home/taylor/PycharmProjects/hakai-ml-train/data/kelp_pa_aco",
+            "/home/taylor/PycharmProjects/hakai-ml-train/checkpoints/kelp_pa",
+            "--name=LRASPP_ACO_DEV",
+            "--num_classes=2",
             "--batch_size=2",
             "--gradient_clip_val=0.5",
             "--accelerator=gpu",
@@ -371,10 +385,10 @@ if __name__ == "__main__":
             "--devices=auto",
             # "--strategy=ddp_find_unused_parameters_false",
             # "--sync_batchnorm",
-            "--max_epochs=10",
-            # '--limit_train_batches=10',
-            # "--limit_val_batches=10",
-            # "--limit_test_batches=10",
+            "--max_epochs=3",
+            '--limit_train_batches=10',
+            "--limit_val_batches=10",
+            "--limit_test_batches=10",
             "--log_every_n_steps=5",
         ])
     else:
