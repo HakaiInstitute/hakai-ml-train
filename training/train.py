24b439bf6c73c81777484097c3a361914fb08592
Taylor Denouden <taylordenouden@gmail.com>
2024-01-19 16:16:46 -0800

    Add flags for training with RGB configs

--- training/train.py
+++ training/train.py
@@ -10,7 +10,9 @@ import wandb
 from pytorch_lightning.loggers import WandbLogger
 from wandb import AlertLevel
 
-from config import TrainingConfig, kelp_pa_efficientnet_b4_config_rgbi, kelp_sp_efficientnet_b4_config_rgbi
+from config import TrainingConfig, kelp_pa_efficientnet_b4_config_rgbi, \
+    kelp_sp_efficientnet_b4_config_rgbi, kelp_sp_efficientnet_b4_config_rgb, \
+    kelp_pa_efficientnet_b4_config_rgb
 from datamodule import DataModule
 from unetplusplus import UNetPlusPlus
 
@@ -107,9 +109,22 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("model_type", type=str, choices=["pa", "sp"])
+    parser.add_argument("band_type", type=str, choices=["rgb", "rgbi"])
     args = parser.parse_args()
-    train_config = {"pa": kelp_pa_efficientnet_b4_config_rgbi, "sp": kelp_sp_efficientnet_b4_config_rgbi}[
-        args.model_type
-    ]
+
+    train_config = None
+    if args.band_type == "rgb":
+        if args.model_type == "pa":
+            train_config = kelp_pa_efficientnet_b4_config_rgb
+        elif args.model_type == "sp":
+            train_config = kelp_sp_efficientnet_b4_config_rgb
+    elif args.band_type == "rgbi":
+        if args.model_type == "pa":
+            train_config = kelp_pa_efficientnet_b4_config_rgbi
+        elif args.model_type == "sp":
+            train_config = kelp_sp_efficientnet_b4_config_rgbi
+
+    if train_config is None:
+        raise ValueError("Invalid model_type or band_type")
 
     train(train_config)
