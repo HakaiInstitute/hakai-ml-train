35214b05cbdb923ee75d3bdc8617176935c9bb37
Taylor Denouden <taylor.denouden@hakai.org>
2025-06-02 13:23:45 -0700

    Working deepness model export

--- train/convert_checkpoint.py
+++ train/convert_checkpoint.py
@@ -1,3 +1,4 @@
+import json
 import os.path
 from collections import OrderedDict
 from pathlib import Path
@@ -73,24 +74,46 @@ def convert_checkpoint(
     dynamic_axes = {
         "input": {
             0: "batch_size",
-            2: "height",
-            3: "width",
+            # 2: "height",
+            # 3: "width",
         },  # Dynamic batch size, height and width
         "output": {
             0: "batch_size",
-            2: "height",
-            3: "width",
+            # 2: "height",
+            # 3: "width",
         },  # Dynamic batch size, height and width
     }
     input_names = ["input"]
     output_names = ["output"]
+
+    class DeepnessModel(torch.nn.Module):
+        def __init__(self, model):
+            super(DeepnessModel, self).__init__()
+            self.model = model
+
+        def forward(self, x):
+            logits = self.model(x)
+            probs = torch.sigmoid(logits)  # Shape [batch_size, 1, height, width]
+
+            # Convert class 1 probabilities to 2 class probs shape: # [batch_size, 2, height, width]
+            probs = torch.cat(
+                [
+                    1 - probs,  # Background class
+                    probs,  # Kelp class
+                ],
+                dim=1,
+            )
+
+            return probs
+
+    model = DeepnessModel(model.model)
 
     torch.onnx.export(
         model,  # Model to export
         x,  # Example input
         output_path_onnx,  # Output file path
         export_params=True,  # Store model weights in the model file
-        opset_version=13,  # ONNX opset version
+        opset_version=15,  # ONNX opset version
         do_constant_folding=True,  # Optimize constants
         input_names=input_names,  # Input tensor names
         output_names=output_names,  # Output tensor names
@@ -98,7 +121,39 @@ def convert_checkpoint(
         verbose=False,
     )
     onnx_model = onnx.load(output_path_onnx)
+
+    class_names = {
+        0: "_background",
+        1: "mussels",
+    }
+
+    m1 = onnx_model.metadata_props.add()
+    m1.key = "model_type"
+    m1.value = json.dumps("Segmentor")
+
+    m2 = onnx_model.metadata_props.add()
+    m2.key = "class_names"
+    m2.value = json.dumps(class_names)
+
+    m3 = onnx_model.metadata_props.add()
+    m3.key = "resolution"
+    m3.value = json.dumps(0.5)  # cm/px
+
+    m4 = onnx_model.metadata_props.add()
+    m4.key = "tiles_overlap"
+    m4.value = json.dumps(40)  # 40% overlap
+
+    m5 = onnx_model.metadata_props.add()
+    m5.key = "standardization_mean"
+    m5.value = json.dumps([0.485, 0.456, 0.406])
+
+    m6 = onnx_model.metadata_props.add()
+    m6.key = "standardization_std"
+    m6.value = json.dumps([0.229, 0.224, 0.225])
+
+    onnx.save(onnx_model, output_path_onnx)
     onnx.checker.check_model(onnx_model)
+
     print(
         f"Saved ONNX model with dynamic dimensions to {os.path.abspath(output_path_onnx)}"
     )
