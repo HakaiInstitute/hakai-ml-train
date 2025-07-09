b2679f53578d6819afc6db42477d47c406823394
Taylor Denouden <taylor.denouden@hakai.org>
2025-07-09 15:55:26 -0700

    Create KOM baseline model

--- trainer.py
+++ trainer.py
@@ -1,9 +1,6 @@
 import torch
 from lightning.pytorch.cli import LightningCLI
 
-from src.datamodule import DataModule
-from src.module import KelpSpeciesModel, SMPSegmentationModel
-
 torch.set_float32_matmul_precision("medium")
 
 
@@ -11,10 +8,7 @@ def cli_main():
     """
     Command-line interface to run SMPSegmentationModel with DataModule.
     """
-    cli = LightningCLI(
-        datamodule_class=DataModule,
-        save_config_kwargs={"overwrite": True},
-    )
+    cli = LightningCLI(save_config_kwargs={"overwrite": True})
     return cli
 
 
