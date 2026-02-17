cd7c114edc31852d32d82bed110388554bfcdc09
Taylor Denouden <taylor.denouden@hakai.org>
2026-02-17 13:22:14 -0700

    compile smp models

--- src/models/smp.py
+++ src/models/smp.py
@@ -58,6 +58,8 @@ class SMPBinarySegmentationModel(
             for p in self.model.encoder.parameters():
                 p.requires_grad = False
 
+        self.model = self.model.compile()
+
         self.loss_fn = losses.__dict__[loss](**loss_opts)
 
         if task == "binary":
